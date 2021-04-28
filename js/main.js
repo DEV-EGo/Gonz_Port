62a5cc0315e1a86d82e06cabbaeede7962c0fc56
Edwin <dev.edwing@gmail.com>
2021-04-28 12:58:21 -0400

    add

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+    $nav = $('.nav');
+    $toggleCollapse = $('.toggle-collapse');
+
+    // click event toggle menu
+
+    $toggleCollapse.click(function(){
+        $nav.$toggleClass('collapse');
+    })
+});
