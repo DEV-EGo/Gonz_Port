655ccd5221c0ab1ea78fe17862bc7fc27c7dfd17
Edwin <dev.edwing@gmail.com>
2021-04-30 00:27:06 -0400

    jquery changes

--- js/main.js
+++ js/main.js
@@ -10,6 +10,12 @@ $(document).ready(function(){
 
     // owl carousel for blog
 
-    $('.owl-carousel').owlCarousel();
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout:3000,
+        dots : false,
+        nav:true
+    });
     
 });
