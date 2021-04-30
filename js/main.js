5cf10f9af18954916dcd73274eb60dd38528fd63
Edwin <dev.edwing@gmail.com>
2021-04-30 12:32:06 -0400

    nav arrows

--- js/main.js
+++ js/main.js
@@ -12,11 +12,11 @@ $(document).ready(function(){
 
     $('.owl-carousel').owlCarousel({
         loop: true,
-        autoplay: true,
+        autoplay: false,
         autoplayTimeout:3000,
         dots : false,
         nav:true,
-        navText :[$(".owl-navigation.owl-nav-prev"),$(".owl-navigation .owl-nav-prev")]
+        navText :[$(".owl-navigation .owl-nav-prev"),$(".owl-navigation .owl-nav-next")]
     });
     
 });
